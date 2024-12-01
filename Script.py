dffa3c18b23806d66562d1c4b0a567c052769ece
seherova <ovaseher1@gmail.com>
2024-12-01 11:07:38 +0300

    performed correlation analysis

--- Script.py
+++ Script.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from pathlib import Path
 import seaborn as sns
-
+import matplotlib.pyplot as plt
 # Load the data
 parent_path = Path(__file__).parent.parent
 file_path = parent_path / 'Data_With_Nonsense_Values.xlsx'
@@ -18,7 +18,7 @@ print(summary_stats.to_string())
 
 
 #for col in data.columns:
-#    print(f"{col} unique values:", data[col].unique())
+#print(f"{col} unique values:", data[col].unique())
 
 # Count missing values in each column
 print('Eksik Değerler:')
@@ -60,3 +60,34 @@ data_cleaned = data_cleaned[~((data_cleaned['Yaş'] < (Q1 - multiplier * IQR)) |
 data_cleaned.to_excel(output_Path, index=False)
 print(f"\n###Temizlenmiş veri kaydedildi:{output_Path}###")
 
+
+print("##########################")
+
+
+binary_columns = ["Ateş", "Öksürük", "Yorgunluk", "Nefes Almada Zorluk", "Cinsiyet", "Çıkış Değişkeni", "Kan Basıncı", "Kolesterol Seviyesi"]
+
+binary_map = {
+    'Yes':1, 'No':0,
+    'Male': 1, 'Female': 0,
+    'Positive':1, 'Negative': 0,
+    'Low': -1, 'Normal':0, 'High':1
+}
+
+df_encoded = data_cleaned.copy()
+
+for col in binary_columns:
+   df_encoded[col] = data_cleaned[col].map(binary_map)
+
+output_file = parent_path / "encoded_data.xlsx"
+df_encoded.to_excel(output_file, index=False)
+print(f"Kodlanmış veri {output_file} dosyasına kaydedildi")
+
+correlation_matrix = df_encoded[binary_columns].corr()
+
+print("Korelasyon matrisi")
+print(correlation_matrix)
+
+plt.figure(figsize=(10, 8))
+sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm", cbar=True)
+plt.title("Korelasyon Matrisi Isı Haritası")
+plt.show()
